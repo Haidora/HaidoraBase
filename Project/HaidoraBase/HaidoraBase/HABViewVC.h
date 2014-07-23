3f5c1d55282611bbae58a201a1a27975f7fd7d7a
mrdaios <mrdaios@gmail.com>
2014-07-23 16:42:19 +0800

    add test

---
+++ Project/HaidoraBase/HaidoraBase/HABViewVC.h
@@ -0,0 +1,13 @@
+//
+//  HABViewVC.h
+//  HaidoraBase
+//
+//  Created by DaiLingchi on 14-7-23.
+//  Copyright (c) 2014年 Haidora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HABViewVC : UIViewController
+
+@end
