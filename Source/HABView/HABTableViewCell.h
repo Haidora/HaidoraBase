3396437279d7ba55140d830cdb7ab0c18da74857
mrdaios <mrdaios@gmail.com>
2014-07-23 16:40:54 +0800

    new: add custom separator for HABTableViewCell.

--- Source/HABView/HABTableViewCell.h
+++ Source/HABView/HABTableViewCell.h
@@ -7,12 +7,36 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger,HABTableViewCellSeparatorStyle)
+{
+    HABTableViewCellSeparatorStyleNone = 0,
+    HABTableViewCellSeparatorStyleCustom
+};
 
 @interface HABTableViewCell : UITableViewCell
 
 //for tableView
-@property (nonatomic, weak  ) UITableView *tableView;
-@property (nonatomic, strong) NSIndexPath *indexPath;
+@property (nonatomic, weak) UITableView *tableView;
+
+//for tableviewcell style
+/**
+ *  default is HABTableViewCellSeparatorStyleNone
+ */
+@property (nonatomic, assign) HABTableViewCellSeparatorStyle habSeparatorStyle;
+/**
+ *  default is 0.5f
+ */
+@property (nonatomic, assign) CGFloat       habSeparatorPadding;
+/**
+ *  default is [UIColor grayColor]
+ */
+@property (nonatomic, strong) UIColor       *habSeparatorColor;
+/**
+ *  default is UIEdgeInsetsMake(0, 0, 0, 0)
+ */
+@property (nonatomic, assign) UIEdgeInsets  habSeparatorEdgeInset;
 
 /**
  *  cellIdentifier default is class name.
@@ -41,7 +65,7 @@
 + (id)cellForTableView:(UITableView *)tableView fromNib:(UINib *)nib;
 + (id)cellForTableView:(UITableView *)tableView fromNib:(UINib *)nib withCellIdentifier:(NSString *)cellIdentifier;
 
-- (void)setContent:(id)content withIndexPath:(NSIndexPath *)indexPath;
+- (void)setContent:(id)content;
 /**
  *  tableViewcell复用时回调
  */
--- Source/HABView/HABTableViewCell.m
+++ Source/HABView/HABTableViewCell.m
@@ -8,7 +8,11 @@
 
 #import "HABTableViewCell.h"
 
+#define kHABSeparatorViewTag 20000
+
 @interface HABTableViewCell ()
+
+@property (nonatomic, strong) UIControl *separatorView;
 
 @end
 
@@ -36,7 +40,59 @@
 
 -(void)defaultInit
 {
+    _habSeparatorStyle = HABTableViewCellSeparatorStyleNone;
+    _habSeparatorColor = [UIColor grayColor];
+    _habSeparatorPadding = 0.5;
+    _habSeparatorEdgeInset = UIEdgeInsetsMake(0, 0, 0, 0);
+}
 
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    if (self.habSeparatorStyle != HABTableViewCellSeparatorStyleNone)
+    {
+        UIView *separatorView = [self viewWithTag:kHABSeparatorViewTag];
+        if (!separatorView)
+        {
+            [self.contentView insertSubview:self.separatorView atIndex:0];
+            self.separatorView.tag = kHABSeparatorViewTag;
+            separatorView = self.separatorView;
+        }
+        CGRect preFrame = self.separatorView.frame;
+        separatorView.frame = CGRectMake(_habSeparatorEdgeInset.left,
+                                         CGRectGetHeight(self.contentView.frame)-CGRectGetHeight(preFrame),
+                                         CGRectGetWidth(self.contentView.frame)-_habSeparatorEdgeInset.right,
+                                         CGRectGetHeight(preFrame));
+    }
+}
+
+-(UIControl *)separatorView
+{
+    if (nil == _separatorView)
+    {
+        switch (_habSeparatorStyle)
+        {
+            case HABTableViewCellSeparatorStyleCustom:
+            {
+                _separatorView = [self generateView:_habSeparatorColor height:_habSeparatorPadding];
+            }
+                break;
+            case HABTableViewCellSeparatorStyleNone:
+            default:
+                break;
+        }
+    }
+    return _separatorView;
+}
+
+-(UIControl *)generateView:(UIColor *)color height:(CGFloat)height
+{
+    UIControl *control = [[UIControl alloc]initWithFrame:CGRectMake(0.0f,
+                                                                    CGRectGetHeight(self.contentView.frame)-height,
+                                                                    CGRectGetWidth(self.contentView.frame),
+                                                                    height)];
+    control.backgroundColor = color;
+    return control;
 }
 
 #pragma mark
@@ -78,17 +134,16 @@
         cellID = @"HABTableViewCellStyleValue2";
     else if (style == UITableViewCellStyleSubtitle)
         cellID = @"HABTableViewCellStyleSubtitle";
+    else
+        cellID = @"HABTableViewCellSystem";
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    HABTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
     if (cell == nil)
     {
-        cell = [[self alloc] initWithStyle:style reuseIdentifier:cellID];
+        cell = [[HABTableViewCell alloc] initWithStyle:style reuseIdentifier:cellID];
     }
-    else
-    {
-        [(HABTableViewCell *)cell reset];
-    }
-    ((HABTableViewCell *)cell).tableView = tableView;
+    [cell reset];
+    cell.tableView = tableView;
     return cell;
 }
 
@@ -101,10 +156,7 @@
         NSArray *nibObjects = [nib instantiateWithOwner:nil options:nil];
         cell = nibObjects[0];
     }
-    else
-    {
-        [(HABTableViewCell *)cell reset];
-    }
+    [(HABTableViewCell *)cell reset];
     ((HABTableViewCell *)cell).tableView = tableView;
     
     return cell;
@@ -126,18 +178,14 @@
             }
         }
     }
-    else
-    {
-        [(HABTableViewCell *)cell reset];
-    }
+    [(HABTableViewCell *)cell reset];
     ((HABTableViewCell *)cell).tableView = tableView;
     
     return cell;
 }
 
-- (void)setContent:(id)content withIndexPath:(NSIndexPath *)indexPath
+- (void)setContent:(id)content
 {
-    self.indexPath = indexPath;
 }
 
 - (void)reset
